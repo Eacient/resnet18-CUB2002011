208e6a41dbe2d38d5374e782d6a2e0d95928307b
Eacient <59136261+Eacient@users.noreply.github.com>
2024-06-30 12:16:32 +0800

    swin model

--- model.py
+++ model.py
@@ -1,24 +1,10 @@
-import torch
-from torchvision.models import resnet18
-from torchvision.models import ResNet18_Weights
-import torch.nn as nn
+from torchvision.models import swin_t, resnet34
 
-def get_resnet18(pretrained=False):
-
-  if not pretrained:
-    model = resnet18(num_classes=200)
-    _ = nn.init.kaiming_normal_(model.fc.weight, mode='fan_out')
-    _ = nn.init.zeros_(model.fc.bias)
-
-  else:
-    weights = ResNet18_Weights.verify('IMAGENET1K_V1')
-    state_dict = weights.get_state_dict(progress=True, check_hash=True)
-    _ = state_dict.pop('fc.weight')
-    _ = state_dict.pop('fc.bias')
-
-    model = resnet18(num_classes=200)
-    model.load_state_dict(state_dict, strict=False)
-    _ = nn.init.kaiming_normal_(model.fc.weight, mode='fan_out')
-    _ = nn.init.zeros_(model.fc.bias)
-
-  return model
+def get_model(args):
+    if args.arch == 'resnet':
+        model = resnet34(num_classes=100)
+    elif args.arch == 'swin':
+        model= swin_t(num_classes=100)
+    else:
+        raise NotImplementedError()
+    return model
