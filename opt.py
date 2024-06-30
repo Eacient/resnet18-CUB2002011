119f49822f0820545593fba491e4406a80cc4e70
Eacient <59136261+Eacient@users.noreply.github.com>
2024-06-30 12:17:10 +0800

    cosine lr

--- opt.py
+++ opt.py
@@ -1,37 +1,60 @@
 from torch import optim
 
-def get_param_group(model, lr, pretrained=False):
+def check_keywords_in_name(name, keywords=()):
+    isin = False
+    for keyword in keywords:
+        if keyword in name:
+            isin = True
+    return isin
 
-  def get_base_param(named_params):
-    for t in named_params:
-      if t[0] == 'fc.weight' or t[0] == 'fc.bias':
-        continue
-      else:
-        yield t[1]
+def set_weight_decay(model, skip_list=(), skip_keywords=()):
+    has_decay = []
+    no_decay = []
 
-  if not pretrained:
-    return model.parameters()
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+        if len(param.shape) == 1 or name.endswith(".bias") or (name in skip_list) or \
+                check_keywords_in_name(name, skip_keywords):
+            no_decay.append(param)
+            # print(f"{name} has no weight decay")
+        else:
+            has_decay.append(param)
+    return [{'params': has_decay},
+            {'params': no_decay, 'weight_decay': 0.}]
 
-  base_param = get_base_param(model.named_parameters())
-  fc_param = [model.fc.weight, model.fc.bias]
+def get_opt(args, model):
+    if args.opt == 'sgd':
+        opt = optim.SGD(set_weight_decay(model), lr=args.lr, weight_decay=args.wd, momentum=0.9)
+    elif args.opt == 'adamw':
+        opt = optim.AdamW(set_weight_decay(model), lr=args.lr, weight_decay=args.wd, betas=(0.9, 0.999), eps=1e-8)
+    else:
+        raise NotImplementedError()
+    return opt
 
-  return [{'params': base_param, 'lr': 0.1 * lr},
-    {'params': fc_param},]
+import math
+def warmup_cosine(args):
+    warmup_epochs = args.warmup_epochs
+    num_epochs = args.epochs
+    cosine_epochs = num_epochs - warmup_epochs
+    base_lr = args.base_lr
+    warmup_lr = args.lr
+    min_lr = args.min_lr
 
-def get_opt(param_group, lr=1e-2, weight_decay=1e-4, name='SGD', momentum=0, nesterov=False):
-  if name == 'SGD':
-    return optim.SGD(param_group, lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov)
-  elif name == 'Adam':
-    return optim.Adam(param_group, lr=lr, weight_decay=weight_decay)
-  else:
-    raise NotImplementedError()
+    def warmup_cosine_handler(epoch):
+        if epoch < warmup_epochs:
+            return (base_lr - warmup_lr) * epoch / warmup_epochs + warmup_lr
+        else:
+            return min_lr + 0.5*(base_lr-min_lr)*(1.0+math.cos(math.pi*(epoch-warmup_epochs)/cosine_epochs))
+        
+    return warmup_cosine_handler
 
-def get_scheduler(opt, name='Step', **kwargs):
-  if name == 'Step':
-    return optim.lr_scheduler.StepLR(opt, step_size=kwargs['step_size'], gamma=0.9)
-  elif name == 'Poly':
-    return optim.lr_scheduler.PolynomialLR(opt, max_epoch=kwargs['max_epoch'], power=2)
-  elif name == 'Cosine':
-    return optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0=20, T_mult=4, eta_min=0)
-  elif name == 'rlop':
-    return optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', factor=0.5, patience=4)
+
+def get_lr_sche(args, opt):
+    if args.lr_sche == 'step':
+        lr_sche = optim.lr_scheduler.StepLR(opt, step_size=args.step_size, gamma=args.gamma)
+    elif args.lr_sche == 'cosine':
+        lr_sche = optim.lr_scheduler.LambdaLR(opt, lr_lambda = warmup_cosine(args))
+    else:
+        return NotImplementedError()
+    return lr_sche
